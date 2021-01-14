abb15a99989c3b11d7dea513504e5e42ce6a46ba
Chris <codewalker@hardkernel.com>
2021-01-14 09:29:10 +0900

    ODROID-C2: Make Android.mk for android-6.0.1

--- Android.mk
+++ Android.mk
@@ -1,1 +1,32 @@
-# Empty Android.mk to keep the build system from using the one in upstream's android/jni.
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE:= libusb
+
+LOCAL_SRC_FILES := \
+				libusb/core.c \
+				libusb/descriptor.c \
+				libusb/hotplug.c \
+				libusb/io.c \
+				libusb/sync.c \
+				libusb/strerror.c \
+				libusb/os/linux_usbfs.c \
+				libusb/os/poll_posix.c \
+				libusb/os/threads_posix.c \
+				libusb/os/linux_netlink.c
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_C_INCLUDES := \
+					$(LOCAL_PATH)/libusb \
+					$(LOCAL_PATH)/libusb/os \
+					$(LOCAL_PATH)/android \
+
+LOCAL_SHARED_LIBRARIES += libc liblog
+
+#LOCAL_CFLAGS += -DOS_LINUX
+
+LOCAL_PRELINK_MODULE := false
+
+include $(BUILD_SHARED_LIBRARY)
